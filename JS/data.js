3c736a51b431d3ac63a777cbef6fd897f924e71d
Slashass <70883938+Slashass@users.noreply.github.com>
2020-09-30 11:53:24 +0300

    data js content

---
+++ JS/data.js
@@ -0,0 +1,25 @@
+/* posto autoriaus icon
+posto autoriaus vardas ir pavarde
+posto parasymo laikas
+posto turinys - textas, nuotraukos, fonas 
+*/
+
+const posts = [
+     {
+        author: {
+            name: 'vardas',
+            surname: 'pavarde',
+            avatar: './img/kazas.jpg',
+            link: 'kazkur linkas'
+        },
+        time: '2019/07/16 16:48:07',
+        content: {
+            text: 'labas rytas',
+            images: [
+                './img/asddsda/jpg',
+            ],
+            background: 'red'
+        },
+    }
+];
+
