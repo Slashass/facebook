72218328a9af754fc3155718713b2a4105059daa
Slashass <70883938+Slashass@users.noreply.github.com>
2020-09-30 12:36:55 +0300

    import export

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+const { default: renderPosts } = require("./render");
+import posts from './data.js';
+import renderPosts from './render.js';
+
+
+renderPosts(posts);
---
+++ JS/render.js
@@ -0,0 +1,7 @@
+function renderPosts(data) {
+    concole.log('gavau duomenis');
+    console.log(data);
+
+}
+
+export default renderPosts;
