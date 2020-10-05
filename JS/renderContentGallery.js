291cb1bb138e1793224cae81f59301306671e39a
Slashass <70883938+Slashass@users.noreply.github.com>
2020-10-05 12:45:23 +0300

    gallery adjestment

--- JS/renderContentGallery.js
+++ JS/renderContentGallery.js
@@ -1,6 +1,27 @@
-function renderContentGallery() {
-    return `<div class="gallery">
-                IMAGES
+function renderContentGallery(photoArray) {
+    const maxGallarySize = 4;
+
+    // jei nera duotas foto sarasas
+    if (!photoArray) {
+        return '';
+    }
+
+let photosHTML = '';    
+for (let i=0; i<photoArray.length;i++) {
+    if (i>=maxGallarySize) {
+        break;
+    }
+    photosHTML += `<img src="./img/posts/${photoArray[i]}" alt="user post photos">`;
+}
+
+    let moreHTML = 0;
+    if (photoArray.length > maxGallarySize) {
+        moreHTML = `<div class="morePhotos">+${photoArray.length - maxGallarySize}</div>`;
+    }
+
+    return `<div class="gallery gallery-${photoArray.length > maxGallarySize ? maxGallarySize : photoArray.length}">
+            ${photosHTML}
+            ${moreHTML}
             </div>`;
 }
 
--- JS/renderContentText.js
+++ JS/renderContentText.js
@@ -1,4 +1,27 @@
 function renderContentText(text) {
-    return `<p>${text}</p>`;
+    const shortTextLength = 50;
+    const normalTextLength = 300;
+    const largeTextLength = 10000;
+    const textLength = text.length;
+
+    
+    if (textLength<=shortTextLength){
+        // kaip labai mazai teksto padidinam texta
+        return `<p class="big-font">${text}</p>`;
+    } else if (textLength<=normalTextLength) {
+        // kai normaliai texto tiesiog avaizduojam
+        return `<p>${text}</p>`;
+    } else if (textLength<=largeTextLength) {
+        // kai labai daug nukerpam irpridedam see more...
+        let shortText = '';
+        for (let i=0; i<normalTextLength;i++) {
+            shortText += text[i];
+        }
+        return `<p>${shortText}...<span class="seeMore">See more</span></p>`;
+    } else {
+        // jei teksto daugiau nei leidziama 
+        console.warn(`virsyta max (${10000}) texto riba`);
+        return '';
+    }
 }
 export default renderContentText;
